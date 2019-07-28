fbddc625a078a0cc82702cf2771fd2920881d690
herteleo <leonard.hertel@gmail.com>
2019-07-28 22:23:25 +0200

    chore: structure + rollup config improvements

--- packages/vue-mu/rollup.config.js
+++ packages/vue-mu/rollup.config.js
@@ -1,7 +1,16 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
 import babel from 'rollup-plugin-babel';
 import common from 'rollup-plugin-commonjs';
+import replacePlugin from 'rollup-plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
-import { version } from './package.json';
+import { uglify } from 'rollup-plugin-uglify';
+
+import {
+  dependencies,
+  peerDependencies,
+  version,
+} from './package.json';
 
 const yearNow = new Date().getFullYear();
 const devYears = [2019];
@@ -15,43 +24,91 @@ const banner = `/*!
  */
 `;
 
+const babelConfig = {
+  babelrc: false,
+  exclude: 'node_modules/**',
+  presets: [
+    [
+      '@babel/env',
+      {
+        modules: false,
+        targets: '> 1%, last 2 versions, not ie <= 8',
+      },
+    ],
+  ],
+};
+
+const replace = ({ silent = false } = {}) => replacePlugin({
+  __VERSION__: JSON.stringify(version),
+  __SILENT__: silent,
+});
+
+const input = () => ({
+  input: 'src/main.js',
+});
+
+const output = (suffix, format, globals) => ({
+  output: {
+    file: `dist/vue-mu.${suffix}.js`,
+    format,
+    name: 'VueMu',
+    banner,
+    globals,
+  },
+});
+
 export default [
   {
-    input: 'lib/vue-mu.js',
-    output: {
-      file: 'dist/vue-mu.common.js',
-      format: 'cjs',
-      name: 'VueMu',
-      banner,
-    },
+    ...input(),
+    ...output('common', 'cjs'),
+    external: [
+      ...Object.keys(dependencies),
+      ...Object.keys(peerDependencies),
+    ],
     plugins: [
-      babel(),
+      replace(),
+      babel(babelConfig),
     ],
   },
   {
-    input: 'lib/vue-mu.js',
-    output: {
-      file: 'dist/vue-mu.esm.js',
-      format: 'es',
-      name: 'VueMu',
-      banner,
-    },
+    ...input(),
+    ...output('esm', 'es'),
+    external: [
+      ...Object.keys(dependencies),
+      ...Object.keys(peerDependencies),
+    ],
     plugins: [
-      babel(),
+      replace(),
+      babel(babelConfig),
     ],
   },
   {
-    input: 'lib/vue-mu.js',
-    output: {
-      file: 'dist/vue-mu.min.js',
-      format: 'umd',
-      name: 'VueMu',
-      banner,
-    },
+    ...input(),
+    ...output('umd', 'umd', { vue: 'Vue' }),
+    external: Object.keys(peerDependencies),
     plugins: [
+      replace(),
       resolve(),
       common(),
-      babel(),
+      babel(babelConfig),
+    ],
+  },
+  {
+    ...input(),
+    ...output('umd.min', 'umd', { vue: 'Vue' }),
+    external: Object.keys(peerDependencies),
+    plugins: [
+      replace({
+        silent: true,
+      }),
+      resolve(),
+      common(),
+      babel(babelConfig),
+      uglify({
+        output: {
+          comments: /^!/,
+        },
+      }),
     ],
   },
 ];
